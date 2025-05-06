2d1a1e1f104576b86becae46ac075f1e8b0bc000
Kelly Mahlangu <165662785+kellymahlangu@users.noreply.github.com>
2025-05-06 19:50:25 +0200

    Update to postgresql

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -2,7 +2,7 @@
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
 import { sql } from "drizzle-orm";
-import { index, sqliteTableCreator } from "drizzle-orm/sqlite-core";
+import { index, pgTableCreator } from "drizzle-orm/pg-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -10,18 +10,18 @@ import { index, sqliteTableCreator } from "drizzle-orm/sqlite-core";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = sqliteTableCreator((name) => `potion_${name}`);
+export const createTable = pgTableCreator((name) => `potion_${name}`);
 
 export const posts = createTable(
   "post",
   (d) => ({
-    id: d.integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
-    name: d.text({ length: 256 }),
+    id: d.serial().primaryKey(),
+    name: d.varchar("name", { length: 256 }),
     createdAt: d
-      .integer({ mode: "timestamp" })
-      .default(sql`(unixepoch())`)
+      .timestamp()
+      .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
-    updatedAt: d.integer({ mode: "timestamp" }).$onUpdate(() => new Date()),
+    updatedAt: d.timestamp().$onUpdate(() => new Date()),
   }),
   (t) => [index("name_idx").on(t.name)]
 );
